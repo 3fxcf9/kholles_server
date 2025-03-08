e175f82d296ab52f0dd96cf6f74a9dc50df56614
3fxcf9 <123954477+3fxcf9@users.noreply.github.com>
2025-03-09 00:56:05 +0100

    Styling changes

--- static/theme.js
+++ static/theme.js
@@ -10,8 +10,10 @@ function setTheme(themeName) {
 setTheme(localStorage.getItem("theme") || themeCycle[0]);
 
 const toggleButton = document.createElement("div");
+
 toggleButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="4"></circle><path d="M12 2v2"></path><path d="M12 20v2"></path><path d="m4.93 4.93 1.41 1.41"></path><path d="m17.66 17.66 1.41 1.41"></path><path d="M2 12h2"></path><path d="M20 12h2"></path><path d="m6.34 17.66-1.41 1.41"></path><path d="m19.07 4.93-1.41 1.41"></path></svg>`;
-toggleButton.addEventListener("click", (e) => {
+
+toggleButton.addEventListener("click", (_) => {
   setTheme(
     themeCycle[
       (themeCycle.indexOf(localStorage.getItem("theme")) + 1) %
@@ -19,14 +21,15 @@ toggleButton.addEventListener("click", (e) => {
     ],
   );
 });
-toggleButton.style = `
-  position: fixed;
-  top: 2rem;
-  right: 2rem;
-  z-index: 1000;
-  cursor:pointer;
-  box-shadow: var(--bg-color) 0 0 15px 7px;
-  background-color: var(--bg-color);
-  `;
-console.log(toggleButton);
+
+toggleButton.id = "theme-selector";
+
 document.body.appendChild(toggleButton);
+
+// Element styling
+const link = document.createElement("link");
+link.href = "/static/styles/theme_selector.css";
+link.type = "text/css";
+link.rel = "stylesheet";
+
+document.getElementsByTagName("head")[0].appendChild(link);
